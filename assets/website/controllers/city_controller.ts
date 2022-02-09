311c8d56770279936df166f7c22075a2636cd458
BlackScorp <cccpmik@gmail.com>
2022-02-09 22:12:08 +0100

    fix camera view

--- assets/website/controllers/city_controller.ts
+++ assets/website/controllers/city_controller.ts
@@ -3,31 +3,48 @@ import * as THREE from 'three'
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 
 
-
 export default class extends Controller {
     connect() {
         let width = this.element.clientWidth;
-        let height =this.element.clientHeight;
-        let aspectRatio = width/height;
+        let height = this.element.clientHeight;
+        let aspectRatio = width / height;
         let loader = new GLTFLoader();
         let renderer = new THREE.WebGLRenderer({alpha: true});
+        let size = 20;
+
         let camera:THREE.Camera;
+
         let scene = new THREE.Scene();
 
-        renderer.setSize(width,height);
+        renderer.setSize(width, height);
         renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.shadowMap.enabled = true;
+        renderer.outputEncoding = THREE.sRGBEncoding;
 
         this.element.appendChild(renderer.domElement);
 
 
         let scenePath = this.element.getAttribute('data-scene');
+        let centerPoint: THREE.Object3D;
+
+        let hemi = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+        hemi.position.set(0, 100, 50);
+        scene.add(hemi);
+
         loader.load(scenePath, (object) => {
 
                 scene.add(object.scene);
 
                 camera = object.cameras[0];
-                console.log(camera,object);
-                camera.aspect = aspectRatio;
+
+                camera.left = size * aspectRatio / -2;
+                camera.right = size * aspectRatio /2;
+                camera.top = size / 2;
+                camera.bottom = size / -2;
+
+
+                centerPoint = object.scene.getObjectByName("Empty");
+                camera.lookAt(centerPoint.position);
                 camera.updateProjectionMatrix();
                 renderer.render(scene, camera);
 
