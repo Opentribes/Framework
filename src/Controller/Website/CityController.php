a793b99d0e66ddb12615e6119fb3a12dd7326380
BlackScorp <cccpmik@gmail.com>
2022-03-16 22:06:39 +0100

    use session username

--- src/Controller/Website/CityController.php
+++ src/Controller/Website/CityController.php
@@ -10,9 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sulu\Bundle\CommunityBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
 final class CityController extends AbstractController
@@ -24,12 +22,7 @@ final class CityController extends AbstractController
     #[Route(path: '/city/{locationX<\d+>}/{locationY<\d+>}')]
     public function viewAction(Request $request): Response
     {
-        $user = $this->getUser();
-        if (!$user instanceof UserInterface) {
-            throw new HttpException(403);
-        }
-        $message = new HttpCityMessage($request, $user);
-
+        $message = new HttpCityMessage($request);
 
         $this->displayBuildingSlots->execute($message);
 
