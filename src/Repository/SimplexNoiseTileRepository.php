aae5adaad5940b540dd5bed3ed301e88bcce4675
cccpmik@gmail.com <cccpmik@gmail.com>
2022-06-25 21:39:29 +0200

    use new tilefactory

--- src/Repository/SimplexNoiseTileRepository.php
+++ src/Repository/SimplexNoiseTileRepository.php
@@ -10,51 +10,38 @@ use OpenTribes\Core\Entity\TileCollection;
 use OpenTribes\Core\Repository\MapTileRepository;
 use OpenTribes\Core\Utils\Location;
 use OpenTribes\Core\Utils\Viewport;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 final class SimplexNoiseTileRepository implements MapTileRepository
 {
 
 
-    public function __construct(private Noise2D $noiseGenerator)
+    public function __construct(private readonly Noise2D $noiseGenerator, private readonly TileFactory $tileFactory, private Stopwatch $stopwatch)
     {
     }
 
     public function findByViewport(Viewport $viewport): TileCollection
     {
         $tileCollection = new TileCollection();
-
+        $this->stopwatch->start('generating-map');
         for ($positionY = $viewport->getMinimumY(); $positionY <= $viewport->getMaximumY(); $positionY++) {
             for ($positionX = $viewport->getMinimumX(); $positionX <= $viewport->getMaximumX(); $positionX++) {
                 $tileLocation = new Location($positionX, $positionY);
                 $tileId = 'position_' . $positionY . '_' . $positionX;
-                $tile = new Tile($tileId, '0000', $tileLocation);
+                $tile = $this->tileFactory->create(id: $tileId, location: $tileLocation);
                 if ($positionX < 0 || $positionY < 0) {
                     $tileCollection[] = $tile;
                     continue;
                 }
 
                 $greyValue = $this->noiseGenerator->getGreyValue($positionX, $positionY);
-
-                $tile = $this->convertGreyValueToTile($greyValue, $tileId, $tileLocation);
+                $tileData = $this->tileFactory->getDataForGreyValue($greyValue);
+                $tile = $this->tileFactory->create(id: $tileId, data: $tileData, location: $tileLocation);
                 $tileCollection[] = $tile;
             }
         }
+        $this->stopwatch->stop('generating-map');
 
         return $tileCollection;
-    }
-
-    private function convertGreyValueToTile(int $greyValue, string $tileId, Location $location): Tile
-    {
-        $data = '0002';
-
-
-        if ($greyValue > 60) {
-            $data = '0001';
-        }
-        if ($greyValue > 190) {
-            $data = '0003';
-        }
-
-        return new Tile($tileId, $data, $location);
     }
 }
